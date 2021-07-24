17f34b2f2a7f663a91ee0db54831faa319f6674c
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-07-24 12:01:38 -0400

    instantiated express app and connected to MongoDB database

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+// we want to limit image sizes being sent
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://treasures:treasures123@treasures.c4a7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// prevents console warnings
+mongoose.set("useFindAndModify", false);
