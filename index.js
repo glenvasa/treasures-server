536d5c9dfef9a54dd5a3a369751007736403891b
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-07-29 16:11:54 -0400

    minor change

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-app.use('/posts', postRoutes)
 
 // we want to limit image sizes being sent
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://treasures:treasures123@treasures.c4a7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
